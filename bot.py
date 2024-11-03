116a96ed9ae5e434049da1d085518236820bd575
Likor7 <sov30082003@gmail.com>
2024-11-03 11:37:34 +0200

    Added deep_link

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from aiogram import Dispatcher, Bot, types
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.deep_linking import create_start_link
 
 from dotenv import load_dotenv
 
@@ -14,22 +15,31 @@ from service.bot_cmds_list import private
 
 from handlers.user_private import user_private_router
 
-ALLOWED_UPDATES=['message, edites_message']
+ALLOWED_UPDATES = ["message, edites_message"]
 
 storage = MemoryStorage()
 dp = Dispatcher()
-bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(
+    token=os.getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 
 dp.include_router(user_private_router)
+
 
 async def main():
     print("bot started")
 
-    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
-    
+    link = await create_start_link(bot, "start", encode=True)
+    print(link)
+
+    await bot.set_my_commands(
+        commands=private, scope=types.BotCommandScopeAllPrivateChats()
+    )
+
     await bot.delete_webhook(drop_pending_updates=True)
 
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
