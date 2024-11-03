1566cae397ed971520bf8f0cb89b9f7fc5f6a4ea
Likor7 <sov30082003@gmail.com>
2024-11-03 11:30:12 +0200

    Prettier wokred

--- middleware/MessageMiddleware.py
+++ middleware/MessageMiddleware.py
@@ -5,17 +5,20 @@ from aiogram.types import TelegramObject
 
 from typing import Callable, Dict, Any, Awaitable
 
-from service.save_data import save_user_data 
+from service.save_data import save_user_data
+
 
 class UserMessageMiddleware(BaseMiddleware):
-    async def __call__(self, 
-                       handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-                       event:  TelegramObject,
-                       data: Dict[str, Any]) -> Any:
-        
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+
         user = data.get("event_from_user")
         if not user:
-            return await handler(event, data) # Перевірка на те чи є в data - user
+            return await handler(event, data)  # Перевірка на те чи є в data - user
 
         user_id = user.id
         user_name = user.username
@@ -23,15 +26,14 @@ class UserMessageMiddleware(BaseMiddleware):
         processing_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         log_data = {
-                "Date/time": f"{processing_time}",
-                "ID": f"{user_id}",
-                "User_Name": f"{user_name}",
-                "User_Message": f"{user_message}",
-            }
-        
+            "Date/time": f"{processing_time}",
+            "ID": f"{user_id}",
+            "User_Name": f"{user_name}",
+            "User_Message": f"{user_message}",
+        }
+
         response = await handler(event, data)
 
         save_user_data(user_id, log_data)
 
         return response
-
